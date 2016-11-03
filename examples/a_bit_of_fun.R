c0ea5e7d2a44dd4d0f2ec7e48f4d38b226d8118a
colin millar <colinpmillar@gmail.com>
2016-11-03 10:15:08 +0100

    move demo to examples and split

--- examples/a_bit_of_fun.R
+++ examples/a_bit_of_fun.R
@@ -1,137 +1,3 @@
-
-# -----------------------------------------------------------------
-# -----------------------------------------------------------------
-#
-#
-#   R and ICES Web services  demo
-#
-#   3rd November 2017
-#
-# -----------------------------------------------------------------
-# -----------------------------------------------------------------
-
-
-
-# -----------------------------
-#  Web services
-# -----------------------------
-
-
-# what is a web service? --------------------------------------
-
-url <- "https://datras.ices.dk/WebServices/DATRASWebService.asmx/getSurveyList"
-
-
-# how to use a web service? --------------------------------------
-
-url <- "https://datras.ices.dk/WebServices/DATRASWebService.asmx/getSurveyList"
-
-# store the response from the url in 'txt'
-txt <- RCurl::getURL(url)
-
-# what is in txt
-txt
-
-# what IS txt
-mode(txt)
-length(txt)
-
-# convert to data.frame
-ret <- XML::xmlToDataFrame(txt)
-
-# what is in ret
-ret
-
-# what IS ret
-str(ret)
-
-# since it is just a vector we could just keep the survey column
-ret$Survey
-
-# Enter icesDatras ---------------------------------
-
-icesDatras::getSurveyList()
-
-
-
-
-# -----------------------------
-#  ICES Web services
-# -----------------------------
-
-# DATRAS --------------------------------
-
-hh <- icesDatras::getHHdata("NS-IBTS", 2015, 1)
-View(hh)
-
-
-# SAG ----------------------------------
-
-sag <- icesSAG::getListStocks(2015)
-View(sag)
-
-# SAG is based on keys
-# lets look at north sea haddock in 2015
-sag <- subset(sag, grepl("Haddock", StockDescription) & EcoRegion == "North Sea")
-key <- 6589 # or key <- sag$key
-
-# get stock summary table -----------------------------
-summary_table <- icesSAG::getSummaryTable(key)
-head(summary_table, 10)
-
-# get a standard graph -------------------------------
-landings_graph <- icesSAG:::getLandingsGraph(key)
-plot(landings_graph)
-
-
-# SLD ----------------------------------
-
-sld <- icesSLD::getSLD()
-View(sld)
-
-url <- "http://stocklist.ices.dk/services/odata4/StockListDWs4"
-
-sld <- jsonlite::fromJSON(url)$value
-
-
-
-#  querying sld VIA the webservice
-# ---------------------------------
-
-# ask for herring stocks from WGBFAS
-query <- "$filter=contains(StockCode, 'had') and EG eq 'WGNSSK'"
-query <- gsub(" ", "%20", query)
-
-sld <- jsonlite::fromJSON(paste0(url, "?", query))$value
-View(sld)
-
-# ask for all TSA assessments
-query <- "$filter=contains(AssessmentType, 'TSA')"
-query <- gsub(" ", "%20", query)
-
-sld <- jsonlite::fromJSON(paste0(url, "?", query))$value
-
-# which EGs and which species?
-table(sld$EG, sld$SpeciesCommonName)
-
-
-# Vocab ----------------------------------
-
-type_list <- icesVocab::getCodeTypeList()
-View(type_list)
-
-subset(type_list, grepl("aphia", tolower(Description)))
-
-aphias <- icesVocab::getCodeList("SpecWorms")
-View(aphias)
-
-subset(aphias, grepl("haddock", tolower(LongDescription)))
-aphiaID <- aphias$Key[grep("haddock", tolower(aphias$LongDescription))]
-
-
-
-
-
 # a bit of fun ----------------------------------
 
 hh <- icesDatras::getDATRAS("HH", "NS-IBTS", c(2000, 2001), 1)
@@ -208,8 +74,8 @@ proj4string(hh) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs8
 tmp <- gContains(area, hh, byid = TRUE)
 area <- area[colSums(tmp) > 0,]
 statrec <- statrec[statrec$Max_Area %in%
-                          gsub(".NA", "",
-                               paste(area$SubArea, area$Division, area$SubDivisio, sep = ".")),]
+                     gsub(".NA", "",
+                          paste(area$SubArea, area$Division, area$SubDivisio, sep = ".")),]
 
 # predict over statsquares that have been sampled at least once ? twice?
 tmp <- gContains(statrec, hh, byid = TRUE)
@@ -235,8 +101,8 @@ statrec$fStatRec <- factor(statrec$StatRec)
 
 # fit
 g2000 <- gam(npue ~ s(fStatRec, bs = "mrf", xt = list(penalty = Q), k = 50),
-           data = subset(data.frame(hh), Year == 2000),
-           drop.unused.levels = FALSE)
+             data = subset(data.frame(hh), Year == 2000),
+             drop.unused.levels = FALSE)
 
 g2001 <- gam(npue ~ s(fStatRec, bs = "mrf", xt = list(penalty = Q), k = 50),
              data = subset(data.frame(hh), Year == 2001),
@@ -289,11 +155,6 @@ tab
 length <- as.numeric(colnames(tab))/10
 plot(length, tab["2000",], type = "h", ylab = "", las = 1, ylim = c(0, max(tab, na.rm=TRUE)))
 points(length + 0.2, tab["2001",], type = "h", col = "red")
-
-
-
-
-
 
 
 
